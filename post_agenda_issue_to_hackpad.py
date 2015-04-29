ae304fb6ab8315832bed3f86b9e85acdeb5a34c0
haphut <haphut@gmail.com>
2015-04-29 15:28:00 +0300

    Fix formatting of pads.
    Thanks a lot, @juyrjola, for fixing the formatting. Hackpad needs some
    workarounds right now.

--- post_agenda_issue_to_hackpad.py
+++ post_agenda_issue_to_hackpad.py
@@ -17,7 +17,11 @@ DECISIONS_HEADERS = {
 
 HACKPAD_BASE_URL = 'https://tomaatti.hackpad.com'
 HACKPAD_PAD_CREATE_URL = HACKPAD_BASE_URL + '/api/1.0/pad/create'
+HACKPAD_PAD_UPDATE_URL = HACKPAD_BASE_URL + '/api/1.0/pad/{}/content'
+# Test pad.
+HACKPAD_PAD_ID = '8PrU6rJdOj7'
 
+# Test hackpad site.
 HACKPAD_CLIENT_ID = 'CpcCZ5uwAkw'
 HACKPAD_CLIENT_SECRET = '1460Qt5KF9Up6cVqy6q7uL1z4sqOiRE6'
 
@@ -54,12 +58,40 @@ def encode_utf8(string):
     return string.encode('utf-8', 'replace')
 
 
+def create_hackpad_heading(string, heading_level=1):
+    arr = None
+    if heading_level == 1:
+        arr = ['<h2>', string, '</h2>']
+    elif heading_level == 2:
+        arr = ['<b>', string, '</b>']
+    elif heading_level == 3:
+        arr = ['<undefined><li><b>', string, '</b></li></undefined>']
+    else:
+        raise ValueError('heading_level must be an integer on the interval '
+                         '[1,3].')
+    return ''.join(arr)
+
+
 def transform_document_to_pad(document):
-    arr = [''.join(['<h1>', document['subject'], '</h1>'])]
+    # The title is sent on the first line as plaintext, separate from the html
+    # content.
+    subject = document['subject']
+    arr = [subject]
+    arr.append('<html><body>')
+    arr.append('<h1>' + subject + '</h1>')
+    # Workaround for a bug in Hackpad. "</h1><h2>" does not work as expected.
+    arr.append('<p></p>')
     for content_item in document['content']:
-        arr.append(''.join(['<h2>', CONTENT_TYPES[content_item['type']],
-                            '</h2>']))
-        arr.append(content_item['text'])
+        in_english = content_item['type']
+        header = in_english
+        try:
+            header = CONTENT_TYPES[in_english]
+        except KeyError:
+            print(in_english, 'in document', document['id'],
+                  'is not a recognized content type.', file=sys.stderr)
+        arr.append(create_hackpad_heading(header, heading_level=2))
+        arr.append(content_item['text'].replace('</p>', '</p><br/>'))
+    arr.append('</body></html>')
     pad = '\n'.join(arr)
     return encode_utf8(pad)
 
@@ -67,13 +99,16 @@ def transform_document_to_pad(document):
 def post_to_hackpad(pad):
     hackpad = OAuth1Session(HACKPAD_CLIENT_ID,
                             client_secret=HACKPAD_CLIENT_SECRET)
-    r = hackpad.post(HACKPAD_PAD_CREATE_URL, data=pad, headers=HACKPAD_HEADERS)
+    # url = HACKPAD_PAD_CREATE_URL
+    url = HACKPAD_PAD_UPDATE_URL.format(HACKPAD_PAD_ID)
+    r = hackpad.post(url, data=pad, headers=HACKPAD_HEADERS)
     r.raise_for_status()
 
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
+
     document = read_openahjo()
     pad = transform_document_to_pad(document)
     post_to_hackpad(pad)
